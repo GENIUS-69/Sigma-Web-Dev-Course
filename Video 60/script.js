59519352cb178ba71a1e2fadfd7ffe230aa9d2d1
GENIUS-69 <galaxygaming0511@gmail.com>
2024-10-17 12:02:43 +0530

    sigma

--- Video 60/script.js
+++ Video 60/script.js
@@ -10,4 +10,14 @@ console.log(a.length);
 console.log(a.concat(b));
 console.log(`His name is ${a} and his friend name is ${b}`);
 
-event
+let c = "Rohan\"Hello"
+console.log(c);
+
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+
+console.log(a.slice(1,5));
+
+console.log(a.replace("en",77));
+
+console.log(a);
